1bd4da57d34980ae52f1d56e3cdcae460201177d
tsbonev <tsbonev@gmail.com>
2018-05-01 13:50:43 +0300

    Add tdd-exercise project

---
+++ tests/com/tsb/MainTest.java
@@ -0,0 +1,15 @@
+package com.tsb;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+
+        assertTrue(false);
+
+    }
+}
